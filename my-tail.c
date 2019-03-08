3bb43640ff0f1c2517dc534ed86a6892bc3d1170
Tomasz Skorupa <2433416+roverslim@users.noreply.github.com>
2019-03-07 20:32:27 -0500

    fixup! wip - my implementation of tail

--- my-tail.c
+++ my-tail.c
@@ -1,18 +1,31 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
-enum { SIZE = 1 };
+enum {
+  BLOCK_COUNT = 1,
+  BLOCK_SIZE = 1 * sizeof(char)
+};
 
 void
-tail (FILE* fp) {
+tail (FILE* fp, int num_lines_wanted) {
     char *buffer;
+    FILE *bof;
+    int num_lines;
     long offset;
  
-    offset = 0;
-    buffer = malloc(sizeof(char));
+    num_lines = offset = 0;
+    buffer = malloc(BLOCK_SIZE);
 
-    while(offset > -10) {
-        if (fseek(fp, offset, SEEK_END) != 0) {
+    // When the input is seekable,
+    //  1- go to the end
+    //  2- read a block
+    //  3- find out how many line breaks it contains
+    //  4- repeat steps 2 & 3 until N line breaks are found
+    //  5- read to standard output from the Nth line break
+
+    while(1) {
+        if (fseek(fp, offset * BLOCK_SIZE, SEEK_END) != 0) {
             if (ferror(fp)) {
                 puts("I/O error when reading");
                 exit(EXIT_FAILURE);
@@ -20,11 +33,20 @@ tail (FILE* fp) {
             // what to do about fseek() returning non-zero and ferror(fp) being falsey
         }
 
-        if (fread(buffer, sizeof(char), SIZE, fp) == SIZE) {
-            printf("%s", buffer);
+        if (fread(buffer, BLOCK_SIZE, BLOCK_COUNT, fp) == BLOCK_COUNT) {
+            if (strcmp("\n", buffer) == 0) {
+                num_lines++;
+
+                if (num_lines > num_lines_wanted) {
+                  int c;
+                  while ((c = fgetc(fp)) != EOF)
+                    putchar(c);
+                  break;
+                }
+            }
         } else {
             if (feof(fp)) {
-                printf("Reading error: unexpected end of file\n");
+                // No-op: we start at the end of the file
             } else if (ferror(fp))
                 perror("Reading error");
         }
@@ -42,7 +64,7 @@ main (void) {
         exit(EXIT_FAILURE);
     }
 
-    tail(fp);
+    tail(fp, 10);
     fclose(fp);
 
     return EXIT_SUCCESS;
