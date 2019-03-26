d60367921f0187e8b18272f07d90da7300279ce5
Tomasz Skorupa <2433416+roverslim@users.noreply.github.com>
2019-03-26 19:48:14 -0400

    Move enum into header file

--- include/tail_n.h
+++ include/tail_n.h
@@ -2,4 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 
+enum {
+  BLOCK_COUNT = 1,
+  BLOCK_SIZE = 1 * sizeof(char)
+};
+
 void tail_n(FILE* fp, int num_lines_wanted);
--- src/tail_n.c
+++ src/tail_n.c
@@ -1,10 +1,5 @@
 #include "tail_n.h"
 
-enum {
-  BLOCK_COUNT = 1,
-  BLOCK_SIZE = 1 * sizeof(char)
-};
-
 void
 tail_n(FILE* fp, int num_lines_wanted) {
     char *buffer;
