0ed7c5f5958a5a4fd846157fc0e42f31cf1a56c2
Tomasz Skorupa <2433416+roverslim@users.noreply.github.com>
2019-03-31 20:17:55 -0400

    Rewrite tail-n to read one character at a time
    It's easier to reason about tail one character at a time.

--- include/tail_n.h
+++ include/tail_n.h
@@ -2,9 +2,4 @@
 #include <stdlib.h>
 #include <string.h>
 
-enum {
-  BLOCK_COUNT = 1,
-  BLOCK_SIZE = 1 * sizeof(char)
-};
-
 void tail_n(FILE* fp, int num_lines_wanted);
--- src/tail_n.c
+++ src/tail_n.c
@@ -9,17 +9,16 @@ display_content(FILE *fp) {
 
 void
 tail_n(FILE *fp, int num_lines_wanted) {
-    char *buffer;
     int num_lines;
     long offset;
-    long int beginning_pos;
 
     if (num_lines_wanted < 0)
       perror("Must specify 0 or more lines to tail");
+    else if (num_lines_wanted == 0)
+      return;
  
     num_lines = 0;
     offset = -1;
-    buffer = malloc(BLOCK_SIZE);
 
     // When the input is seekable,
     //  1- go to the end
@@ -27,35 +26,30 @@ tail_n(FILE *fp, int num_lines_wanted) {
     //  3- find out how many line breaks it contains
     //  4- repeat steps 2 & 3 until N line breaks are found
     //  5- read to standard output from the Nth line break
-    fseek(fp, offset * BLOCK_SIZE, SEEK_SET);
-    beginning_pos = ftell(fp);
 
     while(1) {
-        if (fseek(fp, offset * BLOCK_SIZE, SEEK_END) != 0) {
-            if (ferror(fp)) {
-                puts("I/O error when reading");
-                exit(EXIT_FAILURE);
-            }
-            // what to do about fseek() returning non-zero and ferror(fp) being falsey
-        }
-
-        if (num_lines > num_lines_wanted || beginning_pos == ftell(fp)) {
+        if (num_lines > num_lines_wanted) {
             display_content(fp);
             break;
         }
 
-        if (fread(buffer, BLOCK_SIZE, BLOCK_COUNT, fp) == BLOCK_COUNT) {
-            if (strcmp("\n", buffer) == 0)
-                num_lines++;
-        } else {
-            if (feof(fp)) {
-                puts("Arrived at end of file");
+        if (fseek(fp, offset * sizeof(char), SEEK_END) != 0) {
+            if (ferror(fp)) {
+                perror("I/O error when reading");
                 break;
-            } else if (ferror(fp))
-                perror("Reading error");
+            }
+            // what to do about fseek() returning non-zero and ferror(fp) being falsey
         }
+
+        int c;
+        if ((c = fgetc(fp)) == EOF) {
+            perror("Error reading the next character from the given input stream");
+            break;
+        } else {
+            if (c == '\n')
+                num_lines++;
+        }
+
         offset--;
     }
-
-    free(buffer);
 }
