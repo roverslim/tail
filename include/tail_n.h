02bcd6324990e0dd443795394d97058f53b821cb
Tomasz Skorupa <2433416+roverslim@users.noreply.github.com>
2019-04-13 16:27:21 -0400

    Add include guard
    I intend to use types defined in tail_n.h in parse_arguments.c. In order to do so, I will need to include tail_n.h from multiple places. I add include guards to avoid multiple definitions of the same types.

--- include/tail_n.h
+++ include/tail_n.h
@@ -1,3 +1,5 @@
+#ifndef TAIL_N_H
+#define TAIL_N_H
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -6,3 +8,4 @@ enum direction { RELATIVE_TO_BEGINNING, RELATIVE_TO_END };
 typedef enum direction direction_t;
 
 int tail_n(FILE* fp, int num_lines_wanted, direction_t direction);
+#endif /* TAIL_N_H */
