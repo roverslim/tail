c3c0fdfdd77b7ac1d89e312ebd0947130fac62c2
Tomasz Skorupa <2433416+roverslim@users.noreply.github.com>
2019-04-14 15:59:33 -0400

    Realign code with 4 spaces as tabs

--- src/parse_arguments.c
+++ src/parse_arguments.c
@@ -3,63 +3,62 @@
 arguments_t *
 parse_arguments(int argc, char **argv)
 {
-  char *bvalue, *cvalue, *nvalue;
-  char *file;
-  int fflag, Fflag, rflag;
-  int c;
-  arguments_t *args;
-  direction_t ndirection;
+    char *bvalue, *cvalue, *nvalue;
+    char *file;
+    int fflag, Fflag, rflag;
+    int c;
+    arguments_t *args;
+    direction_t ndirection;
 
-  file = bvalue = cvalue = nvalue = NULL;
-  fflag = Fflag = rflag = 0;
-  /* Supress printing of error messages by getopt */
-  opterr = 0;
+    file = bvalue = cvalue = nvalue = NULL;
+    fflag = Fflag = rflag = 0;
+    /* Supress printing of error messages by getopt */
+    opterr = 0;
 
-  while ((c = getopt (argc, argv, "b:c:fFn:r")) != -1) {
-    switch (c)
-      {
-      case 'b':
-        bvalue = optarg;
-        break;
-      case 'c':
-        cvalue = optarg;
-        break;
-      case 'f':
-        fflag = 1;
-        break;
-      case 'F':
-        Fflag = 1;
-        break;
-      case 'n':
-        nvalue = optarg;
-        break;
-      case 'r':
-        rflag = 1;
-        break;
-      case '?':
-        if (optopt == 'b' || optopt == 'c' || optopt == 'n')
-          fprintf (stderr, "Option -%c requires an argument.\n", optopt);
-        else if (isprint (optopt))
-          fprintf (stderr, "Unknown option `-%c'.\n", optopt);
-        else
-          fprintf (stderr,
-                   "Unknown option character `\\x%x'.\n",
-                   optopt);
-        return NULL;
-      default:
-        abort ();
-      }
-  }
+    while ((c = getopt(argc, argv, "b:c:fFn:r")) != -1) {
+        switch (c) {
+            case 'b':
+                bvalue = optarg;
+                break;
+            case 'c':
+                cvalue = optarg;
+                break;
+            case 'f':
+                fflag = 1;
+                break;
+            case 'F':
+                Fflag = 1;
+                break;
+            case 'n':
+                nvalue = optarg;
+                break;
+            case 'r':
+                rflag = 1;
+                break;
+            case '?':
+                if (optopt == 'b' || optopt == 'c' || optopt == 'n')
+                fprintf (stderr, "Option -%c requires an argument.\n", optopt);
+                else if (isprint (optopt))
+                fprintf (stderr, "Unknown option `-%c'.\n", optopt);
+                else
+                fprintf (stderr,
+                         "Unknown option character `\\x%x'.\n",
+                         optopt);
+                return NULL;
+            default:
+            abort ();
+        }
+    }
 
-  ndirection = RELATIVE_TO_END;
-  if ((nvalue != NULL) && (strncmp("+", nvalue, 1) == 0))
-      ndirection = RELATIVE_TO_BEGINNING;
+    ndirection = RELATIVE_TO_END;
+    if ((nvalue != NULL) && (strncmp("+", nvalue, 1) == 0))
+        ndirection = RELATIVE_TO_BEGINNING;
 
-  args = arguments_init(
-      file,
-      bvalue, cvalue, nvalue,
-      ndirection,
-      fflag, Fflag, rflag
-  );
-  return args;
+    args = arguments_init(
+        file,
+        bvalue, cvalue, nvalue,
+        ndirection,
+        fflag, Fflag, rflag
+    );
+    return args;
 }
