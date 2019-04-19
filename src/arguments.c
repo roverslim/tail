25b5b6de90a1471822335af88f3ce58ad0192592
Tomasz Skorupa <2433416+roverslim@users.noreply.github.com>
2019-04-19 14:13:42 -0400

    Align arguments.c with 4 spaces as tabs

--- src/arguments.c
+++ src/arguments.c
@@ -2,21 +2,21 @@
 #include "tail_n.h"
 
 enum {
-  DEFAULT_N_VALUE = 10
+    DEFAULT_N_VALUE = 10
 };
 
 struct flags {
-  int f;
-  int F;
-  int r;
+    int f;
+    int F;
+    int r;
 };
 
 struct values {
-  char *b;
-  char *c;
+    char *b;
+    char *c;
 
-  int n;
-  direction_t ndirection;
+    int n;
+    direction_t ndirection;
 };
 
 struct arguments {
@@ -27,7 +27,7 @@ struct arguments {
 
 int
 arguments_get_n(arguments_t *args) {
-  return args->values.n;
+    return args->values.n;
 };
 
 direction_t
@@ -42,72 +42,72 @@ arguments_init(
     direction_t ndirection,
     int f, int F, int r
 ) {
-  arguments_t *args;
+    arguments_t *args;
 
-  args = malloc(sizeof(arguments_t));
-  if (args == NULL)
-    return NULL;
+    args = malloc(sizeof(arguments_t));
+    if (args == NULL)
+        return NULL;
 
-  args->file = NULL;
-  if (file != NULL) {
-    args->file = malloc(sizeof(file) + 1);
-    if (args->file == NULL) {
-      arguments_free(args);
-      return NULL;
+    args->file = NULL;
+    if (file != NULL) {
+        args->file = malloc(sizeof(file) + 1);
+        if (args->file == NULL) {
+            arguments_free(args);
+            return NULL;
+        }
+
+        args->file = strcpy(args->file, file);
     }
 
-    args->file = strcpy(args->file, file);
-  }
+    args->flags.f = f;
+    args->flags.F = F;
+    args->flags.r = r;
 
-  args->flags.f = f;
-  args->flags.F = F;
-  args->flags.r = r;
+    args->values.b = NULL;
+    if (b != NULL) {
+        args->values.b = malloc(sizeof(b) + 1);
+        if (args->values.b == NULL) {
+            arguments_free(args);
+            return NULL;
+        }
 
-  args->values.b = NULL;
-  if (b != NULL) {
-    args->values.b = malloc(sizeof(b) + 1);
-    if (args->values.b == NULL) {
-      arguments_free(args);
-      return NULL;
+        args->values.b = strcpy(args->values.b, b);
     }
 
-    args->values.b = strcpy(args->values.b, b);
-  }
+    args->values.c = NULL;
+    if (c != NULL) {
+        args->values.c = malloc(sizeof(c) + 1);
+        if (args->values.c == NULL) {
+            arguments_free(args);
+            return NULL;
+        }
 
-  args->values.c = NULL;
-  if (c != NULL) {
-    args->values.c = malloc(sizeof(c) + 1);
-    if (args->values.c == NULL) {
-      arguments_free(args);
-      return NULL;
+        args->values.c = strcpy(args->values.c, c);
     }
 
-    args->values.c = strcpy(args->values.c, c);
-  }
+    if (n != NULL)
+        args->values.n = abs(atoi(n));
+    else
+        args->values.n = DEFAULT_N_VALUE;
 
-  if (n != NULL)
-    args->values.n = abs(atoi(n));
-  else
-    args->values.n = DEFAULT_N_VALUE;
+    args->values.ndirection = ndirection;
 
-  args->values.ndirection = ndirection;
-
-  return args;
+    return args;
 };
 
 void
 arguments_free(arguments_t *args) {
-  if (args == NULL)
+    if (args == NULL)
+        return;
+
+    if (args->file != NULL)
+        free(args->file);
+
+    if (args->values.b != NULL)
+        free(args->values.b);
+    if (args->values.c != NULL)
+        free(args->values.c);
+
+    free(args);
     return;
-
-  if (args->file != NULL)
-    free(args->file);
-
-  if (args->values.b != NULL)
-    free(args->values.b);
-  if (args->values.c != NULL)
-    free(args->values.c);
-
-  free(args);
-  return;
 };
