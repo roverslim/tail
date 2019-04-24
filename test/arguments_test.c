104aedc9f09b50228affebd137c809d65c69d024
Tomasz Skorupa <2433416+roverslim@users.noreply.github.com>
2019-04-23 20:46:32 -0400

    Additional tests for arguments.c
    Test arguments_get_n_direction()

--- test/arguments_test.c
+++ test/arguments_test.c
@@ -20,9 +20,23 @@ test_arguments_get_n_returns_int(void) {
     arguments_free(args);
 }
 
+void
+test_arguments_get_ndirection_returns_direction_t(void) {
+    arguments_t *args;
+
+    args = arguments_init(NULL, NULL, NULL, NULL, RELATIVE_TO_BEGINNING, 0, 0, 0);
+    TEST_ASSERT_EQUAL(RELATIVE_TO_BEGINNING, arguments_get_ndirection(args));
+    arguments_free(args);
+
+    args = arguments_init(NULL, NULL, NULL, NULL, RELATIVE_TO_END, 0, 0, 0);
+    TEST_ASSERT_EQUAL(RELATIVE_TO_END, arguments_get_ndirection(args));
+    arguments_free(args);
+}
+
 int
 main(void) {
     UNITY_BEGIN();
     RUN_TEST(test_arguments_get_n_returns_int);
+    RUN_TEST(test_arguments_get_ndirection_returns_direction_t);
     return UNITY_END();
 }
